0c9a42a95213f8f7f393bc97dd366d9b862d97de
Sorawee Porncharoenwase <nullzero.free@gmail.com>
2014-09-09 18:33:06 -0400

    x

--- query.py
+++ query.py
@@ -1,5 +1,5 @@
 from sqlalchemy import *
-from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref, subqueryload
 from sqlalchemy.ext.declarative import declarative_base
 
 URI = 'mysql://thwiki.labsdb/thwiki_p?read_default_file=~/replica.my.cnf'
@@ -65,7 +65,7 @@ ts90days = tots(datetime.datetime.now() - datetime.timedelta(days=1))
 ts30days = tots(datetime.datetime.now() - datetime.timedelta(days=1))
 
 dic = {}
-users = User.query.filter(User.user_registration >= ts90days).all()
+users = User.query.options(subqueryload(User.revisions)).filter(User.user_registration >= ts90days).all()
 print 'start the loop'
 
 import operator
@@ -78,7 +78,6 @@ with open('query-result.txt', 'w') as f:
             revisions = user.revisions
             for revision in revisions:
                 print revision
-                """
                 if revision.rev_timestamp >= ts30days and revision.page.page_namespace == 0 and revision.rev_deleted == 0:
                     if revision.parent is None:
                         if revision.rev_len >= LENGTH_DIFF:
@@ -86,7 +85,6 @@ with open('query-result.txt', 'w') as f:
                     else:
                         if revision.parent.rev_len - revision.rev_len >= LENGTH_DIFF:
                             cnt += 1
-                """
             if cnt:
                 dic[user.user_name] = cnt
         except Exception as e:
@@ -94,5 +92,5 @@ with open('query-result.txt', 'w') as f:
             print e
             print 'found problem with {}'.format(user.user_name)
 
-    for row in sorted(x.iteritems(), key=operator.itemgetter(1), reverse=True):
+    for row in sorted(dic.iteritems(), key=operator.itemgetter(1), reverse=True):
         f.write('| [[User:{}]] || {}\n'.format(row[0], row[1]))
