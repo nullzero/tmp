07ed56afedf446e0ecd6bcb2a93d04bc30ec286b
tools.nullzerobot <tools.nullzerobot@tools-dev.eqiad.wmflabs>
2014-09-09 22:22:41 +0000

    change

--- query.py
+++ query.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
 URI = 'mysql://thwiki.labsdb/thwiki_p?read_default_file=~/replica.my.cnf'
-URI = 'mysql://root:password@localhost/wikidb'
+# URI = 'mysql://root:password@localhost/wikidb'
 
 engine = create_engine(URI, convert_unicode=True)
 session = scoped_session(sessionmaker(autocommit=False,
@@ -55,6 +55,8 @@ import datetime
 def tots(s):
     return s.strftime("%Y%m%d%H%M%S")
 
+# print Revision.query.filter(Revision.rev_timestamp < "20150816184516").all()
+
 import collections
 
 LENGTH_DIFF = 500
@@ -62,22 +64,35 @@ LENGTH_DIFF = 500
 ts90days = tots(datetime.datetime.now() - datetime.timedelta(days=1))
 ts30days = tots(datetime.datetime.now() - datetime.timedelta(days=1))
 
-cnt = collections.Counter()
+dic = {}
 users = User.query.filter(User.user_registration >= ts90days).all()
-for user in users:
-    print user
-    revisions = user.revisions
-    for revision in revisions:
-        if revision.rev_timestamp >= ts30days and revision.page.page_namespace == 0 and revision.rev_deleted == 0:
-            if revision.parent is None:
-                if revision.rev_len >= LENGTH_DIFF:
-                    cnt[user] += 1
-            else:
-                if revision.parent.rev_len - revision.rev_len >= LENGTH_DIFF:
-                    cnt[user] += 1
+print 'start the loop'
+
+import operator
 
 with open('query-result.txt', 'w') as f:
-    for row in cnt.most_common(500):
-        f.write('| [[User:{}]] || {}\n'.format(row[0].user_name, row[1]))
+    for user in users:
+        cnt = 0
+        print user
+        try:
+            revisions = user.revisions
+            for revision in revisions:
+                print revision
+                """
+                if revision.rev_timestamp >= ts30days and revision.page.page_namespace == 0 and revision.rev_deleted == 0:
+                    if revision.parent is None:
+                        if revision.rev_len >= LENGTH_DIFF:
+                            cnt += 1
+                    else:
+                        if revision.parent.rev_len - revision.rev_len >= LENGTH_DIFF:
+                            cnt += 1
+                """
+            if cnt:
+                dic[user.user_name] = cnt
+        except Exception as e:
+            f.write('found problem with {}'.format(user.user_name))
+            print e
+            print 'found problem with {}'.format(user.user_name)
 
-import os
+    for row in sorted(x.iteritems(), key=operator.itemgetter(1), reverse=True):
+        f.write('| [[User:{}]] || {}\n'.format(row[0], row[1]))
