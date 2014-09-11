700c5af3e26f5882506cb645ccd4a76cf2c60d01
Sorawee Porncharoenwase <nullzero.free@gmail.com>
2014-09-11 13:42:48 -0400

    asd

--- query.py
+++ query.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref, 
 from sqlalchemy.ext.declarative import declarative_base
 
 URI = 'mysql://thwiki.labsdb/thwiki_p?read_default_file=~/replica.my.cnf'
-# URI = 'mysql://root:password@localhost/wikidb'
+URI = 'mysql://root:password@localhost/wikidb'
 
 engine = create_engine(URI, convert_unicode=True)
 session = scoped_session(sessionmaker(autocommit=False,
@@ -59,14 +59,16 @@ def tots(s):
 
 import collections
 
-ts90days = tots(datetime.datetime.now() - datetime.timedelta(days=1))
-ts30days = tots(datetime.datetime.now() - datetime.timedelta(days=1))
-
+current = datetime.datetime(day=1, month=9, year=2014)
+before1month = tots(current - datetime.timedelta(days=30))
+before3month = tots(current - datetime.timedelta(days=90))
 import operator
 
 def find_edit():
     dic = {}
-    users = User.query.options(subqueryload(User.revisions)).filter(User.user_registration >= ts90days).all()
+    users = User.query.options(
+        subqueryload(User.revisions)
+    ).filter(between(User.user_registration, before3month, current)).all()
     print 'start the loop'
     LENGTH_DIFF = 500
     with open('task1.txt', 'w') as f:
@@ -76,7 +78,9 @@ def find_edit():
             try:
                 revisions = user.revisions
                 for revision in revisions:
-                    if revision.rev_timestamp >= ts30days and revision.page.page_namespace == 0 and revision.rev_deleted == 0:
+                    if ((before1month <= revision.rev_timestamp <= current) and
+                            revision.page.page_namespace == 0 and
+                            revision.rev_deleted == 0):
                         if revision.parent is None:
                             if revision.rev_len >= LENGTH_DIFF:
                                 cnt += 1
@@ -94,13 +98,14 @@ def find_edit():
             f.write('| [[User:{}]] || {}\n'.format(row[0], row[1]))
             print row
 
-def find_create():
+"""
+def find_create_rotate():
     cnt = collections.Counter()
-    print 'before q'
     for i in xrange(0, 5600000, 10000):
         print i
-        revisions = Revision.query.filter(between(Revision.rev_id, i, i+10000), Revision.rev_parent_id == 0, Revision.rev_deleted == 0)
-        print 'after q'
+        revisions = Revision.query.filter(between(Revision.rev_id, i, i+10000-1),
+                                          Revision.rev_parent_id == 0,
+                                          Revision.rev_deleted == 0)
         for revision in revisions:
             print revision
             if revision.page and revision.page.page_namespace == 0:
@@ -111,5 +116,23 @@ def find_create():
         for i, j in cnt.most_common(100):
             f.write('|-\n| [[User:{}]] || {}'.format(i, j))
             print '|-\n| [[User:{}]] || {}'.format(i, j)
+"""
 
-find_create()
+def find_create():
+    cnt = collections.Counter()
+    revisions = Revision.query.filter(between(Revision.rev_timestamp, before1month, current),
+                                      Revision.rev_parent_id == 0,
+                                      Revision.rev_deleted == 0)
+    for revision in revisions:
+        print revision
+        if revision.page and revision.page.page_namespace == 0:
+            cnt[revision.rev_user_text] += 1
+    
+    print 'begin writing'
+    with open('task2.txt', 'w') as f:
+        for i, j in cnt.most_common(100):
+            f.write('|-\n| [[User:{}]] || {}'.format(i, j))
+            print '|-\n| [[User:{}]] || {}'.format(i, j)
+
+find_edit()
+# find_create()
